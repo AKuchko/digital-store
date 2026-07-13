a5e8ed538f5a4456ee1bd5e265c2a822f27095ea
AKuchko <akuchko.career@mail.ru>
2026-07-13 10:50:14 +0700

    feat: added styles for base Radio input

--- components/Radio/Radio.tsx
+++ components/Radio/Radio.tsx
@@ -3,6 +3,7 @@ import style from './Radio.module.css'
 type RadioProps = {
     value: string
     name: string
+    checked: boolean
     label?: string
     onChange: (value: string) => void
 }
@@ -10,7 +11,14 @@ type RadioProps = {
 function Radio(props: RadioProps) {
     return (
         <label className={style.radio}>
-            <input className={style.input} type="radio" value={props.value} name={props.name} />
+            <input
+                className={style.input}
+                type="radio"
+                value={props.value}
+                checked={props.checked}
+                name={props.name}
+                onChange={(e) => props.onChange(e.target.value)}
+            />
             {props.label}
         </label>
     )
