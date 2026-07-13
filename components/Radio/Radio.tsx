2224d32d9cd15d3a312b0c95d88c671a2f6438e2
AKuchko <akuchko.career@mail.ru>
2026-07-13 10:58:32 +0700

    fix(radio): added disabled state

--- components/Radio/Radio.tsx
+++ components/Radio/Radio.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx'
 import style from './Radio.module.css'
 
 type RadioProps = {
@@ -5,18 +6,25 @@ type RadioProps = {
     name: string
     checked: boolean
     label?: string
+    disabled?: boolean
     onChange: (value: string) => void
 }
 
 function Radio(props: RadioProps) {
     return (
-        <label className={style.radio}>
+        <label
+            className={clsx(
+                style.radio,
+                props.disabled && style.disabled
+            )}
+        >
             <input
                 className={style.input}
                 type="radio"
                 value={props.value}
                 checked={props.checked}
                 name={props.name}
+                disabled={props.disabled}
                 onChange={(e) => props.onChange(e.target.value)}
             />
             {props.label}
