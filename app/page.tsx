7406c4b4f023d851fb9c139e12afce684017099d
AKuchko <akuchko.career@mail.ru>
2026-08-21 17:58:10 +0700

    feat(i18n): added next-intl library for translations

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,24 @@
 import { Button } from "@/components/Button";
 import Checkbox from "@/components/Checkbox/Checkbox";
 import { useState } from "react";
+import { useTranslations } from "next-intl";
 
 export default function Home() {
     const [checked, setChecked] = useState(false)
 
+    const t = useTranslations('homePage')
+
     return (
         <div className="flex flex-col flex-1 items-center gap-1.5 justify-center bg-zinc-50 font-sans dark:bg-black">
+            <h1>{t('title')}</h1>
+
+            <p>{t('name', { name: 'Brad' })}</p>
+            <p>{t('plural', { count: 0 })}</p>
+            <p>{t('plural', { count: 1 })}</p>
+            <p>{t('plural', { count: 3 })}</p>
+            <p>{t('select', { gender: 'male' })}</p>
+            <p>{t('select', { gender: 'female' })}</p>
+            <p>{t('select', { gender: 'other' })}</p>
             <Button label="Button" />
             <Button label="Button" icon="plus" />
             <Button label="Button" disabled />
